a9cc3bc5cb0ec04883718a6ba30d40a9cf030661
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-05-18 00:14:08 +0200

    Properties form improved

--- src/views/admin/parts/FieldPropertiesFormsTemplates.php
+++ src/views/admin/parts/FieldPropertiesFormsTemplates.php
@@ -1,18 +1,40 @@
 <!-- Field Properties Forms: -->
 <script id="textInputTemplateProperties" type="text/template">
 	<div>
-		<label>Label: <input type="text" name="label" /></label>
-		<br />
-		<label>Label width: <input type="text" name="labelWidth" style="width: 70px" />px</label>
-		<br />
-		Label location: <label><input type="radio" name="labelLocation" value="top" />Top</label><label><input type="radio" name="labelLocation" value="inline" />Inline</label>
-		<br />
-		<label>Label align: <select name="labelAlign"><option value="left">Left</option><option value="center">Center</option><option value="right">Right</option></select></label>
-		<br />
-		<label>Placeholder: <input type="text" name="placeholder" /></label>
-		<br />
-		<label>Required: <input type="checkbox" name="required" /></label>
-		<br />
-		<label>Width: <select name="width"><option value="auto">Auto</option><option value="100%">100%</option></select></label>
+
+		<table>
+			<tr>
+				<td colspan="2"><label for="label">Label: </label></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input id="label" type="text" name="label" class="wfFullWidth" /></td>
+			</tr>
+			<tr>
+				<td><label for="labelWidth">Label width: </label></td>
+				<td><input type="text" id="labelWidth" name="labelWidth" style="width: 70px" />&nbsp;px</td>
+			</tr>
+			<tr>
+				<td><label>Label location: </label></td>
+				<td><label><input type="radio" name="labelLocation" value="top" />Top</label>&nbsp;&nbsp;<label><input type="radio" name="labelLocation" value="inline" />Inline</label></td>
+			</tr>
+			<tr>
+				<td><label for="labelAlign">Label align: </label></td>
+				<td><select id="labelAlign" name="labelAlign"><option value="left">Left</option><option value="center">Center</option><option value="right">Right</option></select></td>
+			</tr>
+			<tr>
+				<td colspan="2"><label for="placeholder">Placeholder:</label></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input id="placeholder" type="text" name="placeholder" class="wfFullWidth" /></td>
+			</tr>
+			<tr>
+				<td><label for="required">Required: </label></td>
+				<td><input id="required" type="checkbox" name="required" /></td>
+			</tr>
+			<tr>
+				<td><label for="width">Width: </label></td>
+				<td><select id="width" name="width"><option value="auto">Auto</option><option value="100%">100%</option></select></td>
+			</tr>
+		</table>
 	</div>
 </script>
