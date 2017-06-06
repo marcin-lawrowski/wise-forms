4b3f2c07a45132d4c1204ef72afc844be5a4195c
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-06-06 17:35:25 +0200

    Text area field rendering

--- src/shortcodes/templates/form/fields/TextArea.php
+++ src/shortcodes/templates/form/fields/TextArea.php
@@ -1,1 +1,20 @@
-<textarea></textarea>
+<div class="<?php if ($labelLocation == 'inline' && strlen($labelWidth) > 0) { ?>wfTable<?php } ?>">
+	<?php if (strlen($label) > 0) { ?>
+		<label for="<?php echo $id; ?>"
+			   class="wfFieldLabel<?php if ($labelLocation == 'inline') { ?> wfCell<?php } ?> wfFieldLabelAlign<?php echo ucfirst($labelAlign); ?>"
+			   style="<?php if (strlen($labelWidth) > 0) { ?>width: <?php echo $labelWidth; ?>px<?php } ?>"
+		>
+			<?php echo $label; ?><?php if ($required) { ?><span class="wfFieldLabelRequiredMark">*</span><?php } ?>
+		</label>
+	<?php } ?>
+
+	<span class="<?php if ($labelLocation == 'inline') { ?>wfCell<?php } ?><?php if ($width == '100%') { ?> wfWidth100<?php } ?>">
+		<textarea id="<?php echo $id; ?>"
+				name="<?php echo $id; ?>"
+				style="<?php if (strlen($height) > 0) { ?>height: <?php echo $height; ?>px<?php } ?>"
+				<?php if ($required) { ?>required<?php } ?>
+				placeholder="<?php echo htmlentities($placeholder, ENT_QUOTES, 'UTF-8'); ?>"
+				class="wfTextArea<?php if ($width == '100%') { ?> wfWidth100<?php } ?>"
+		></textarea>
+	</span>
+</div>
