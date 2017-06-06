db1b76844cb15572b72274e1319742494e251e0b
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-06-06 23:21:08 +0200

    Submit button rendering

--- src/shortcodes/templates/form/fields/ButtonSubmit.php
+++ src/shortcodes/templates/form/fields/ButtonSubmit.php
@@ -1,1 +1,3 @@
-<input type="submit" value="<?php echo htmlentities($label, ENT_QUOTES, 'UTF-8'); ?>" />
+<div class="wfFieldTextAlign<?php echo ucfirst($align); ?>"">
+	<input type="submit" value="<?php echo htmlentities($label, ENT_QUOTES, 'UTF-8'); ?>" class="wfButtonSubmit" />
+</div>
