e0db4cd70ee2dfb94d3ae36267a92b0cb5c6cea1
Marcin Lawrowski <marcin.lawrowski@gmail.com>
2017-06-07 16:54:07 +0200

    Radio buttons rendering

--- src/shortcodes/templates/form/fields/RadioButtons.php
+++ src/shortcodes/templates/form/fields/RadioButtons.php
@@ -1,1 +1,22 @@
-radio0 buttons
+<div class="<?php if ($labelLocation == 'inline' && strlen($labelWidth) > 0) { ?>wfTable<?php } ?>">
+	<?php if (strlen($label) > 0) { ?>
+		<label class="wfFieldLabel<?php if ($labelLocation == 'inline') { ?> wfCell<?php } ?> wfFieldLabelAlign<?php echo ucfirst($labelAlign); ?>"
+			   style="<?php if (strlen($labelWidth) > 0) { ?>width: <?php echo $labelWidth; ?>px<?php } ?>"
+		>
+			<?php echo $label; ?><?php if ($required) { ?><span class="wfFieldLabelRequiredMark">*</span><?php } ?>
+		</label>
+	<?php } ?>
+
+	<span class="<?php if ($labelLocation == 'inline') { ?>wfCell<?php } ?>">
+		<?php if (is_array($options)) { ?>
+			<?php foreach ($options as $key => $option) { ?>
+				<label class="wfRadioButtonsLabel"><input type="radio"
+														id="<?php echo $id; ?>_<?php echo $key; ?>"
+														name="<?php echo $id; ?>"
+														class="wfRadioButtons"
+														value="<?php echo htmlentities($option['key'], ENT_QUOTES, 'UTF-8'); ?>"
+					/><?php echo $option['value']; ?></label>
+			<?php } ?>
+		<?php } ?>
+	</span>
+</div>
